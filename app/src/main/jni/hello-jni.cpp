23f295c19460547c49a3d6e96ec545067a94b413
sandeep shabd <sandeep.shabd@lochbridge.com>
2016-01-10 17:41:19 -0500

    adding log for c++ file

--- app/src/main/jni/hello-jni.cpp
+++ app/src/main/jni/hello-jni.cpp
@@ -5,10 +5,16 @@
 #include "hello-jni.h"
 #include <string.h>
 #include <jni.h>
+#include <stdlib.h>
+
+#include <android/log.h>
+
+#define LOGI(...) ((void)__android_log_print(ANDROID_LOG_INFO, "App2", __VA_ARGS__))
 
 JNIEXPORT jstring
         Java_apps_sandeep_com_ndktestapp1_MainActivity_helloJni( JNIEnv* env,
                                                           jobject thiz ){
-    return env->NewStringUTF( "Hello JNI from c++! ");
+    LOGI( "******Logging the hello world*******" );
+    return env->NewStringUTF( "Hello JNI from c++ !! ");
 
 }
